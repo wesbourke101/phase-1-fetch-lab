ba903a9ec50b0626bf640faa266b89ec5539bb54
Wes Bourke <wesbourkeavl@gmail.com>
2022-03-02 13:37:21 -0500

    commit tree a970814392e70767b368d3a328b233e7b4ea18ab

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books') 
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++')
 }
 
 function renderBooks(books) {
